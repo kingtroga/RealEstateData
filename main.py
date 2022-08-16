f4d42be364c758b576e872d4a65ad7db42223369
Tari Yekorogha <trogaclassicman@gmail.com>
2022-08-16 23:17:10 +0100

    figured out some more logic

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ import pandas as pd
 
 
 
+headers = {
+    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)AppleWebKit 537.36 (KHTML, like Gecko) Chrome',
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'
+    }
+url = 'https://www.whatismybrowser.com/'\
